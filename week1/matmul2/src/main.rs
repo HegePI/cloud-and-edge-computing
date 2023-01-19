64c8f545eeffd29f0470cf9fa1b86f48a7de95d3
Heikki Pulli <heikki.pulli@helsinki.fi>
2023-01-19 16:26:22 +0200

    matmul2 random vecs

--- week1/matmul2/src/main.rs
+++ week1/matmul2/src/main.rs
@@ -1,14 +1,20 @@
+use rand::distributions::Uniform;
+use rand::{self, Rng};
 use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
-    let x = 10000;
+    let x = 1000000;
     let y = 1000;
     let z = 1;
 
-    let a = vec![vec![0.3; y]; x];
-    let b = vec![vec![0.3; x]; y];
-    let c = vec![vec![0.3; z]; x];
+    let mut rng = rand::thread_rng();
+    let range = Uniform::new(0., 1.);
+
+    let a = vec![(0..y).map(|_| rng.sample(&range)).collect::<Vec<f64>>(); x];
+    let b = vec![(0..x).map(|_| rng.sample(&range)).collect::<Vec<f64>>(); y];
+    let c = vec![(0..x).map(|_| rng.sample(&range)).collect::<Vec<f64>>(); z];
+    println!("matrices created");
 
     let mut ab = vec![vec![0.0; x]; x];
     for i in 0..x {
